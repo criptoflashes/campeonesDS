240893fce287ebd77e39d61dd7b134e26b11d613
FloatingRiver8 <maceiramariana@gmail.com>
2023-10-14 17:38:46 -0300

    add try catch in uploadPhotosToCloudinary

--- actions/uploadActions.js
+++ actions/uploadActions.js
@@ -3,10 +3,9 @@ import path from "path";
 import fs from "fs/promises";
 import { v4 as uuidv4 } from "uuid";
 import os from "os";
-import { v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import { revalidatePath } from "next/cache";
 import { createTempDirectory } from "create-temp-directory";
-
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -17,23 +16,19 @@ cloudinary.config({
 async function savePhotosToLocal(formData) {
   const files = formData.getAll("file");
   /*    console.log(files) */
-  
-   const tempDir = await createTempDirectory();
 
-  
+  const tempDir = await createTempDirectory();
+
   const multipleBuffersPromise = files.map((file) =>
     file.arrayBuffer().then((data) => {
       const buffer = Buffer.from(data);
       const name = uuidv4();
       const ext = file.type.split("/")[1];
 
-      
-
-     
       const uploadDir = path.join(tempDir.path, `/${name}.${ext}`);
 
       fs.writeFile(uploadDir, buffer);
-      console.log(uploadDir)
+      console.log(uploadDir);
       return { filepath: uploadDir, filename: file.name };
     })
   );
@@ -42,20 +37,20 @@ async function savePhotosToLocal(formData) {
 }
 
 async function uploadPhotosToCloudinary(newFiles) {
-  const multiplePhotosPromise = newFiles.map((file) =>
-    cloudinary.uploader.upload(file.filepath, { folder: "nextjs_upload" })
-  );
+  try {
+    const multiplePhotosPromise = newFiles.map((file) =>
+      cloudinary.uploader.upload(file.filepath, { folder: "nextjs_upload" })
+    );
 
-  return await Promise.all(multiplePhotosPromise);
+    return await Promise.all(multiplePhotosPromise);
+  } catch (error) {
+    return { errMsg: error.message };
+  }
 }
-
-
-
 
 export async function uploadPhoto(formData) {
   /* console.log(formData);
- */
-
+   */
 
   try {
     const newFiles = await savePhotosToLocal(formData);
@@ -64,32 +59,26 @@ export async function uploadPhoto(formData) {
     const photos = await uploadPhotosToCloudinary(newFiles);
     console.log("photoss", photos);
 
-
-
     newFiles.map((file) => fs.unlink(file.filepath));
 
     return { msg: "upload sucess!" };
-   
- 
+
     revalidatePath("/");
-
-
   } catch (error) {
-    return { errMsg:  error.message };
+    return { errMsg: error.message };
   }
 }
 
-
 export async function getAllPhotos() {
-  
-   try {
-     
- const { resources } = await cloudinary.search.expression('folder:nextjs_upload/*').sort_by('created_at', 'desc').max_results(500).execute()
+  try {
+    const { resources } = await cloudinary.search
+      .expression("folder:nextjs_upload/*")
+      .sort_by("created_at", "desc")
+      .max_results(500)
+      .execute();
 
-return resources
- 
-   } catch (error) {
-     return { errMsg: error.message };
-   }
- }
- 
+    return resources;
+  } catch (error) {
+    return { errMsg: error.message };
+  }
+}
